cdf0346b34ab66e0e6cd6672d97f84e44d36fca9
MindSystem <mindlockreverser@gmail.com>
2020-05-08 20:05:46 +0200

    Added plugin base

---
+++ Plugin Base/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginBase
+{
+    public class Calculator
+    {
+        public static void Execute(MethodDef method)
+        {
+            for (var i = 0; i < method.Body.Instructions.Count; i++)
+            {
+              //Your code :)
+            }
+
+        }
+    }
+}
